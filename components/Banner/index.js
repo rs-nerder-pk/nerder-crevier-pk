a46139f40c8576bc27e16489946735f6cb5ca7f9
Glenhayes <hayes@glen@gmail.com>
2022-04-23 13:43:21 -0600

    add banner component

---
+++ components/Banner/index.js
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+export default function Banner() {
+  return (
+    <div className="grid lg:grid-cols-12">
+      <div className="p-8 col-start-3 col-span-4">
+        <div className="border-blue border-y-20 text-red text-center py-4">
+          icon
+        </div>
+        <div className="text-blue">
+          <h2 className="text-xl font-black">Service Stations</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+            praesentium nihil quas? Voluptate quisquam minima perspiciatis,
+            laborum id voluptas voluptates dicta obcaecati, animi illum
+            repudiandae. Sequi ad error nostrum minima!
+          </p>
+        </div>
+        <div className="relative hidden">
+          <Image
+            src="/images/banner-home.jpeg"
+            alt="banner image"
+            width={1025}
+            height={425}
+            layout="fill"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
