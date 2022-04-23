3cfadd1cdc0621624f07817467b0f9241fed4d5e
Glenhayes <hayes@glen@gmail.com>
2022-04-23 09:11:18 -0600

    add fonts and brand colors

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello Crevier!</h1>
+      <h1 className="text-5xl font-black uppercase text-brand-red border-brand-blue border-y-50">
+        Crevier Fuels
+      </h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,44 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'brand-blue': {
+          DEFAULT: '#213871',
+          50: '#97ACE1',
+          100: '#839CDB',
+          200: '#5C7DCF',
+          300: '#385FC0',
+          400: '#2D4C98',
+          500: '#213871',
+          600: '#1B2E5D',
+          700: '#15244A',
+          800: '#101B36',
+          900: '#0A1122',
+        },
+        'brand-red': {
+          DEFAULT: '#EB0028',
+          50: '#FFA4B3',
+          100: '#FF8FA2',
+          200: '#FF6680',
+          300: '#FF3E5F',
+          400: '#FF153D',
+          500: '#EB0028',
+          600: '#B8001F',
+          700: '#850017',
+          800: '#52000E',
+          900: '#1F0005',
+        },
+      },
+      borderWidth: {
+        20: '20px',
+        50: '50px',
+      },
       gridTemplateRows: {
         layout: 'auto 1fr auto',
       },
+    },
+    fontFamily: {
+      sans: ['Hurme', 'times'],
     },
   },
   plugins: [],
