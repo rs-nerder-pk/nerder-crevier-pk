23983c21d7e16cdcf6149a77e4f44453428398ce
Glen Hayes <glen@nerder.com>
2022-07-19 11:57:54 -0600

    add no stations found section

--- components/stationFinder/List.js
+++ components/stationFinder/List.js
@@ -23,70 +23,81 @@ export default function StationList({ stations, userPosition, resetFilters }) {
   }, [stations, userPosition]);
 
   return (
-    <div className="px-4">
-      <div className="container mx-auto bg-white p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {locations && (
-          <div className="col-span-full flex justify-between">
-            <h2 className="text-sm text-blue-500 font-normal normal-case">
-              Showing {locations.length} Locations
-            </h2>
-            <div className="flex gap-4">
-              <button
-                className="text-sm text-red-500"
-                onClick={() => resetFilters()}
-              >
-                Reset Filters
-              </button>
-            </div>
+    <div className="container mx-auto bg-white p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      {locations && (
+        <div className="col-span-full flex justify-between">
+          <h2 className="text-sm text-blue-500 font-normal normal-case">
+            Showing {locations.length} Locations
+          </h2>
+          <div className="flex gap-4">
+            <button
+              className="text-sm text-red-500"
+              onClick={() => resetFilters()}
+            >
+              Reset Filters
+            </button>
           </div>
-        )}
-        {locations &&
-          locations.map((location) => {
-            // console.log({ location })
-            return (
-              <div
-                key={location.id}
-                className="border-t-20 border-blue-500 py-4"
-              >
-                <h3 className="text-base font-bold text-blue">
-                  {location.title}
-                </h3>
-                <address className="not-italic text-blue leading-normal mt-2 text-sm">
-                  {location.addressLineOne}
-                  <br />
-                  {location.addressLineTwo}
-                  <br />
-                  <a
-                    href={`https://maps.google.com/?q=${location.lat},${location.lng}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-red-500"
-                  >
-                    {locale === "fr"
-                      ? "Ouvrir dans Google map"
-                      : "Open in Google Map"}
-                  </a>
-                  <br />
-                  <a
-                    className="inline-block mt-4 text-red-500"
-                    href={`tel:+${location.tel}`}
-                  >
-                    {location.tel}
-                  </a>
-                </address>
-                {location.features && (
-                  <ul className="grid grid-cols-2 text-sm mt-6 gap-2 ml-0 text-blue-500">
-                    {location.features.map((feature) => (
-                      <li key={feature.id} className="ml-3.5">
-                        {feature.value}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            );
-          })}
-      </div>
+        </div>
+      )}
+
+      {!locations.length && (
+        <div className="col-span-full text-center">
+          <h3 className=" normal-case  leading-snug">No stations found.</h3>
+          <p>
+            Please refine or{" "}
+            <button
+              className="text-sm text-red-500"
+              onClick={() => resetFilters()}
+            >
+              reset filters
+            </button>{" "}
+            filters
+          </p>
+        </div>
+      )}
+      {locations &&
+        locations.map((location) => {
+          // console.log({ location })
+          return (
+            <div key={location.id} className="border-t-20 border-blue-500 py-4">
+              <h3 className="text-base font-bold text-blue">
+                {location.title}
+              </h3>
+              <address className="not-italic text-blue leading-normal mt-2 text-sm">
+                {location.addressLineOne}
+                <br />
+                {location.addressLineTwo}
+                <br />
+                <a
+                  href={`https://maps.google.com/?q=${location.lat},${location.lng}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-red-500"
+                >
+                  {locale === "fr"
+                    ? "Ouvrir dans Google map"
+                    : "Open in Google Map"}
+                </a>
+                <br />
+                <a
+                  className="inline-block mt-4 text-red-500"
+                  href={`tel:+${location.tel}`}
+                >
+                  {location.tel}
+                </a>
+              </address>
+              {location.features && (
+                <ul className="grid grid-cols-2 text-sm mt-6 gap-2 ml-0 text-blue-500">
+                  {location.features.map((feature) => (
+                    <li key={feature.id} className="ml-3.5">
+                      {feature.value}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          );
+        })}
     </div>
   );
 }
