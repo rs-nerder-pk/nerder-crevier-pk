56f0a603151c5deaeca851e35ffa50dee79b626f
Glen Hayes <glen@nerder.com>
2022-07-19 13:57:32 -0600

    update map filters into groups

--- components/stationFinder/Filters.js
+++ components/stationFinder/Filters.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { stationTranslations } from "utils/station-filters";
+import { stationTranslations, stationFilters } from "utils/station-filters";
 
 export default function StationFilters({
-  filters,
   activeFilterIds,
   setActiveFilterIds,
 }) {
@@ -32,11 +31,78 @@ export default function StationFilters({
   };
 
   return (
-    <fieldset>
-      <legend className="text-lg font-bold text-white mb-4">
-        {router.locale === "en-US" ? "Filter By:" : "Filtrer par :"}
-      </legend>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 ">
+    <>
+      {stationFilters && (
+        <div>
+          <h2 className="text-lg font-bold text-white mb-4">
+            {router.locale === "en-US" ? "Filter By:" : "Filtrer par :"}
+          </h2>
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+            {stationFilters.default.filters.map((filter) => (
+              <div key={filter.id} className="relative flex items-start ">
+                <div className="mr-3 flex items-center h-5">
+                  <input
+                    id={`filter-${filter.id}`}
+                    name={`filter-${filter.id}`}
+                    type="checkbox"
+                    checked={isActive(filter.id)}
+                    onChange={() => toggleFilter(filter.id)}
+                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                  />
+                </div>
+                <div className="min-w-0 text-sm">
+                  <label
+                    htmlFor={`filter-${filter.id}`}
+                    className="font-medium text-white select-none"
+                  >
+                    {router.locale === "en-US"
+                      ? filter.value
+                      : stationTranslations(filter.value)}
+                  </label>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+            <FilterGroup
+              group={stationFilters.gas}
+              isActive={isActive}
+              toggleFilter={toggleFilter}
+            />
+            <FilterGroup
+              group={stationFilters.propane}
+              isActive={isActive}
+              toggleFilter={toggleFilter}
+            />
+            <FilterGroup
+              group={stationFilters.carWash}
+              isActive={isActive}
+              toggleFilter={toggleFilter}
+            />
+            <FilterGroup
+              group={stationFilters.quickServeRestaurants}
+              isActive={isActive}
+              toggleFilter={toggleFilter}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+const FilterSectionTitle = ({ children }) => {
+  return <h3 className="text-base font-bold text-white">{children}</h3>;
+};
+
+const FilterGroup = ({ group, isActive, toggleFilter }) => {
+  const { locale } = useRouter();
+  const title = locale === "en-US" ? group.value : group.valueFR;
+  const filters = group.filters;
+  return (
+    <div className="mt-8">
+      <FilterSectionTitle>{title}</FilterSectionTitle>
+      <ul className="grid gap-4 m-0 mt-4">
         {filters.map((filter) => (
           <div key={filter.id} className="relative flex items-start ">
             <div className="mr-3 flex items-center h-5">
@@ -54,14 +120,14 @@ export default function StationFilters({
                 htmlFor={`filter-${filter.id}`}
                 className="font-medium text-white select-none"
               >
-                {router.locale === "en-US"
+                {locale === "en-US"
                   ? filter.value
                   : stationTranslations(filter.value)}
               </label>
             </div>
           </div>
         ))}
-      </div>
-    </fieldset>
+      </ul>
+    </div>
   );
-}
+};
--- utils/station-filters.js
+++ utils/station-filters.js
@@ -50,3 +50,134 @@ export const stationTranslations = (enValue) => {
       return enValue;
   }
 };
+
+const defaultFilters = [
+  {
+    id: null,
+    value: "See All",
+  },
+  {
+    id: 1,
+    value: "With Service",
+  },
+  {
+    id: 2,
+    value: "Full Serve",
+  },
+  {
+    id: 6,
+    value: "Diesel",
+  },
+  {
+    id: 10,
+    value: "Gas Bar",
+  },
+  {
+    id: 11,
+    value: "Convenience Store",
+  },
+  {
+    id: 12,
+    value: "Auto Repair",
+  },
+  {
+    id: 13,
+    value: "Open 24 Hours",
+  },
+  {
+    id: 16,
+    value: "Family Restaurant",
+  },
+  {
+    id: 20,
+    value: "Automatic Bank Machine",
+  },
+  {
+    id: 21,
+    value: "Towing",
+  },
+  {
+    id: 22,
+    value: "Marina",
+  },
+  {
+    id: 23,
+    value: "Car Rental",
+  },
+  {
+    id: 24,
+    value: "Fast-charge stations for electric vehicles",
+  },
+];
+
+const gasFilters = [
+  {
+    id: 4,
+    value: "Premium Unleaded Gasoline",
+  },
+  {
+    id: 5,
+    value: "Super 94 Gasoline",
+  },
+];
+
+const propaneFilters = [
+  {
+    id: 8,
+    value: "Propane for cars",
+  },
+  {
+    id: 9,
+    value: "Propane- Tank Refills or Exchange",
+  },
+];
+
+const carWashFilters = [
+  {
+    id: 14,
+    value: "Hand Car Wash",
+  },
+  {
+    id: 15,
+    value: "Automatic Car Wash",
+  },
+];
+
+const quickServeRestaurantsFilters = [
+  {
+    id: 17,
+    value: "Tim Hortons",
+  },
+  {
+    id: 18,
+    value: "McDonalds",
+  },
+];
+
+export const stationFilters = {
+  default: {
+    filters: defaultFilters,
+    value: "",
+    valueFR: "",
+  },
+  gas: {
+    filters: gasFilters,
+    value: "Gas",
+    valueFR: "Essence",
+  },
+  propane: {
+    filters: propaneFilters,
+    value: "Propane",
+    valueFR: "Propane",
+  },
+  carWash: {
+    filters: carWashFilters,
+    value: "Car Wash",
+    valueFR: "LAVE-AUTO",
+  },
+  quickServeRestaurants: {
+    filters: quickServeRestaurantsFilters,
+    value: "Quick Serve Restaurants",
+    valueFR: "RESTAURATION RAPIDE",
+  },
+};
